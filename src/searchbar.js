a56d0a76289347fd2a4d62bb105aa1bd807c7f67
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-01 15:16:33 -0400

    Update searchbar.js

--- src/searchbar.js
+++ src/searchbar.js
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
+import "./SearchBar.css"; // Import the CSS file
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setQuery(value);
+    onSearch(value); // Send search input to the parent component (Index.js)
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search shows or movies..."
+        value={query}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
 
